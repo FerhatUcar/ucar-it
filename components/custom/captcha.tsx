43454c58d9109befb13356c6bc37d467ead5cf85
Ferhat <ucarferhat3@gmail.com>
2024-01-26 01:36:26 +0100

    add captcha

---
+++ components/custom/captcha.tsx
@@ -0,0 +1,34 @@
+import React, { Dispatch, FC, SetStateAction } from "react";
+import { Input } from "@/components/ui/input";
+import { baseColor } from "@/app/const";
+import { Bot } from "lucide-react";
+
+type CaptchaProps = {
+  captchaValue: string;
+  userInput: string;
+  setUserInput: Dispatch<SetStateAction<string>>;
+};
+
+export const Captcha: FC<CaptchaProps> = ({
+  captchaValue,
+  userInput,
+  setUserInput,
+}) => {
+  const handleInputChange = (e: {
+    target: { value: SetStateAction<string> };
+  }) => {
+    setUserInput(e.target.value);
+  };
+  return (
+    <div>
+      <div className="flex flex-row gap-2 mb-4 text-white items-center">
+        <Bot size={20} color={baseColor} />
+        <span>Are u human?</span>
+      </div>
+      <div className="flex flex-row items-center">
+        <label className="w-[100px]">{captchaValue} =</label>
+        <Input type="text" value={userInput} onChange={handleInputChange} />
+      </div>
+    </div>
+  );
+};
---
+++ utils/captcha.ts
@@ -0,0 +1,5 @@
+export const generateCaptcha = () => {
+  const num1 = Math.floor(Math.random() * 10) + 1;
+  const num2 = Math.floor(Math.random() * 10) + 1;
+  return `${num1} + ${num2}`;
+};

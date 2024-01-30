51d26a4d4a36580906f602ddf1134df578750c7f
Ferhat <ucarferhat3@gmail.com>
2024-01-30 22:36:55 +0100

    update select css

--- components/ui/textarea.tsx
+++ components/ui/textarea.tsx
@@ -10,7 +10,7 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
     return (
       <textarea
         className={cn(
-          "flex outline-0 min-h-[80px] w-full rounded-md border border-input focus-visible:border-2 focus:border-rose-500 bg-background " +
+          "flex outline-0 min-h-[80px] w-full rounded-md border border-input focus:ring-0 focus-visible:border-2 focus:border-rose-500 bg-background " +
             "focus:ring-0 px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-ring " +
             "disabled:cursor-not-allowed disabled:opacity-50",
           className
